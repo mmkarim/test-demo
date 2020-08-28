323cbefa14c767c569696eb2671cc5ac0823e6f3
mmkarim <mksaikat123@gmail.com>
2020-08-28 12:44:05 +0200

    Add some smtp config for productin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  host = ENV['SMTP_HOST']
+  config.action_mailer.default_url_options = { host: host }
+
+  config.action_mailer.smtp_settings = {
+    :address              => ENV['SMTP_ADDRESS'],
+    :port                 => ENV['SMTP_PORT'],
+    :user_name            => ENV['SMTP_USERNAME'],,
+    :password             => ENV['SMTP_PASSWORD'],,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
