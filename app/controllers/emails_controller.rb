d7323c710289533747ec41b4edc9044f370ce550
mmkarim <mksaikat123@gmail.com>
2020-08-28 12:29:16 +0200

    Deliver email after successful submit

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -26,6 +26,8 @@ class EmailsController < ApplicationController
 
     respond_to do |format|
       if @email.save
+        DemoMailer.default_email(**email_params.to_h.symbolize_keys).deliver_later
+
         format.html { redirect_to @email, notice: 'Email was successfully created.' }
         format.json { render :show, status: :created, location: @email }
       else
